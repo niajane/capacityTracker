61a3e876e9a2cfb562e83c8c2dd8c56b9238b69d
Nia Gardner <5283851+niajane@users.noreply.github.com>
2021-10-14 17:00:39 +0100

    remove console.log, testData, commented out code no longer needed

--- client/my-app/src/api/index.js
+++ client/my-app/src/api/index.js
@@ -1,21 +1,7 @@
 import axios from 'axios';
 
-//const url = process.env.DB_URL1 ==null ? "" : process.env.DB_URL1.replace(';','');
-//console.log(url);
-
 const url = "http://localhost:8000/count"
 axios.defaults.baseURL = ``;
-//console.log(url.replace(';',''));
 
-//export const createList = (list: Object) : Promise<AxiosResponse<List>> => axios.post(url, list);
 export const getItems = () => axios.get(url);
 export const getAverage = () => axios.get(`${url}/average`);
-/*export const getListsOverview = () : Promise<AxiosResponse> => axios.get(`${url}/overview`);
-export const getList = (id: string) : Promise<AxiosResponse> => axios.get(`${url}/${id}`);
-export const updateList = (list_id: string, changes: Object) : Promise<AxiosResponse> => axios.patch(`${url}/${list_id}`, changes);
-export const deleteList = (id: string) : Promise<AxiosResponse> => axios.delete(`${url}/${id}`);
-export const deleteCompleted = (list: string) : Promise<AxiosResponse> => axios.delete(`${url}/completed/${list}`);
-export const deleteTodo = (list_id: string, todo_id: string) : Promise<AxiosResponse> => axios.delete(`${url}/${list_id}/${todo_id}`);
-export const updateTodo = (list_id: string, todo_id: string, changes: Object) : Promise<AxiosResponse> => axios.patch(`${url}/${list_id}/${todo_id}`, changes);
-export const createTodo = (list_id: string, todo: Object) : Promise<AxiosResponse> => axios.post(`${url}/${list_id}`, todo);
-*/
